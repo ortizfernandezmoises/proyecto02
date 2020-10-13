fafe503fedbe4b1c2323ff6b492f219d7b8342d9
ortizfernandezmoises <72079357+ortizfernandezmoises@users.noreply.github.com>
2020-10-13 13:43:25 +0200

    Update Metodosfind.js

--- Metodosfind.js
+++ Metodosfind.js
@@ -4,7 +4,7 @@ db.ordenadoress.find ({"objeto": {$eq: "Mac"}})
 db.ordenadoress.find ({"cantidad": {$gt: 25}})
 db.ordenadoress.find ({ estado: {$in:["D", "N"]}})
 db.ordenadoress.find( { estado: "D", cantidad: { $lt: 19 } } )
-db.ordenadoress.find( { $or: [ { estado: "N" }, { qty: { $lt: 25 } } ] } )
+db.ordenadoress.find( { $or: [ { estado: "N" }, { cantidad: { $lt: 25 } } ] } )
 db.ordenadoress.find( {
     estado: "N",
     $or: [ { cantidad: { $lt: 30 } }, { objeto: /^p/ } ]
